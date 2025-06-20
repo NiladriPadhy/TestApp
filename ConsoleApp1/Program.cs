f45645c3accbcc7a0a244e0478aaa8259d6ecb36
Niladri Padhy <niladri.1437@gmail.com>
2025-06-20 14:53:43 +0530

    Initial comit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.LIB;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            new Class1().CallMethod();
+            Console.ReadLine();
+        }
+    }
+}
